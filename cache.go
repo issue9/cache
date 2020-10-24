f46701a30327fa365449dd0ae61dcff6dfcdf40b
caixw <xnotepad@gmail.com>
2020-10-24 13:00:13 +0800

    feat: 添加一些常用的常量

--- cache.go
+++ cache.go
@@ -5,8 +5,21 @@ package cache
 
 import "errors"
 
-// Forever 永不过时
-const Forever = 0
+// 常用时间的定义
+const (
+	Forever     = 0 //  永不过时
+	OneMinute   = 60
+	FiveMinutes = 5 * OneMinute
+	TenMinutes  = 10 * OneMinute
+	HalfHour    = 30 * OneMinute
+	OneHour     = 60 * OneMinute
+	HalfDay     = 12 * OneHour
+	OneDay      = 24 * OneHour
+	OneWeek     = 7 * OneDay
+	ThirtyDays  = 30 * OneDay // 30 天
+	SixtyDays   = 60 * OneDay // 60 天
+	NinetyDays  = 90 * OneDay // 90 天
+)
 
 // ErrCacheMiss 当不存在缓存项时返回的错误
 var ErrCacheMiss = errors.New("cache: 未找到缓存项")
